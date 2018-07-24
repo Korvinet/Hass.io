27230a19c999526be5423d3df36f805fe75919dd
korvi <jm.martinez@korvi.net>
2018-07-24 14:01:44 -0500

    add new api test.

--- lutron_pico/run.sh
+++ lutron_pico/run.sh
@@ -11,4 +11,5 @@ PWD="$(jq --raw-output '.password' $CONFIG_PATH)"
 echo "Password: $PWD"
 echo "API password: $HASSIO_TOKEN"
 echo 'Starting service...'
+curl -X GET -H "x-ha-access: Chopan88" -H "Content-Type: application/json" http://192.168.2.75:8123/api/
 python3 /lutron_pico.py --host $HOST --port $PORT --user $USER --pwd $PWD --apipwd $HASSIO_TOKEN
