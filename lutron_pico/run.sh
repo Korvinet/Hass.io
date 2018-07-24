5b95b5dac62f327d4eed29ffd3584a9ea27b96c3
korvi <jm.martinez@korvi.net>
2018-07-24 10:39:30 -0500

    Add spawn to telnet call.

--- lutron_pico/run.sh
+++ lutron_pico/run.sh
@@ -1,4 +1,4 @@
-echo 'Build version: 1.0.12'
+echo 'Build version: 1.0.13'
 echo 'Configuring plugin...'
 CONFIG_PATH=/data/options.json
 HOST="$(jq --raw-output '.host' $CONFIG_PATH)"
@@ -11,5 +11,6 @@ PWD="$(jq --raw-output '.password' $CONFIG_PATH)"
 echo "Password: $PWD"
 echo "API password: $HASSIO_TOKEN"
 echo 'Starting telnet connection...'
-telnet $HOST $PORT
+spawn telnet $HOST $PORT
+
 #python3 /lutron_pico.py --host $HOST --port $PORT --user $USER --pwd $PWD --apipwd $HASSIO_TOKEN
