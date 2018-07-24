17263012aca0bdeac8308a5d362ac2b157cb6ded
korvi <jm.martinez@korvi.net>
2018-07-24 00:16:04 -0500

    Test curl for lutron pico.

--- lutron_pico/run.sh
+++ lutron_pico/run.sh
@@ -1,3 +1,4 @@
+echo 'Build version: 1.0.2'
 echo 'Configuring plugin...'
 CONFIG_PATH=/data/options.json
 HOST="$(jq --raw-output '.host' $CONFIG_PATH)"
@@ -9,5 +10,7 @@ echo "Username: $USER"
 PWD="$(jq --raw-output '.password' $CONFIG_PATH)"
 echo "Password: $PWD"
 echo "API password: $HASSIO_TOKEN"
+echo "Testing api..."
+curl -X POST -H "x-ha-access: $HASSIO_TOKEN" -H "Content-Type: application/json" -d '{"device_id": 17, "button_id": 8 }' http://hassio/homeassistant/api/events/lutron_button_pressed
 echo 'Starting service...'
 python3 /lutron_pico.py --host $HOST --port $PORT --user $USER --pwd $PWD --apipwd $HASSIO_TOKEN
