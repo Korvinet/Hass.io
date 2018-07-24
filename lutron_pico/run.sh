04c70bc25816b893e9cbb20405ce7007a3a38153
korvi <jm.martinez@korvi.net>
2018-07-24 10:05:41 -0500

    Test curl call with no slash, no content type and spaces removed.

--- lutron_pico/run.sh
+++ lutron_pico/run.sh
@@ -1,4 +1,4 @@
-echo 'Build version: 1.0.7'
+echo 'Build version: 1.0.8'
 echo 'Configuring plugin...'
 CONFIG_PATH=/data/options.json
 HOST="$(jq --raw-output '.host' $CONFIG_PATH)"
@@ -13,7 +13,6 @@ echo "API password: $HASSIO_TOKEN"
 echo "Testing api..."
 
 curl -X GET -H "x-ha-access: $HASSIO_TOKEN" -H "Content-Type: application/json" http://hassio/homeassistant/api/
-
-curl -X POST -H "x-ha-access: $HASSIO_TOKEN" -H "Content-Type: application/json" -d '{"device_id": 17, "button_id": 8 }' http://hassio/homeassistant/api/events/lutron_button_pressed/
+curl -X POST -H "x-ha-access: $HASSIO_TOKEN" -H "Content-Type: application/json" -d '{"device_id":17, button_id":8}' http://hassio/homeassistant/api/events/lutron_button_pressed
 echo 'Starting service...'
 python3 /lutron_pico.py --host $HOST --port $PORT --user $USER --pwd $PWD --apipwd $HASSIO_TOKEN
