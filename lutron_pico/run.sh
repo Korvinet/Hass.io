c43f615d4c615733bd1a138e60ff4780c444a01a
korvi <jm.martinez@korvi.net>
2018-07-24 10:34:05 -0500

    add telnet installation.

--- lutron_pico/run.sh
+++ lutron_pico/run.sh
@@ -1,4 +1,4 @@
-echo 'Build version: 1.0.11'
+echo 'Build version: 1.0.12'
 echo 'Configuring plugin...'
 CONFIG_PATH=/data/options.json
 HOST="$(jq --raw-output '.host' $CONFIG_PATH)"
@@ -11,7 +11,5 @@ PWD="$(jq --raw-output '.password' $CONFIG_PATH)"
 echo "Password: $PWD"
 echo "API password: $HASSIO_TOKEN"
 echo 'Starting telnet connection...'
-
 telnet $HOST $PORT
-
-python3 /lutron_pico.py --host $HOST --port $PORT --user $USER --pwd $PWD --apipwd $HASSIO_TOKEN
+#python3 /lutron_pico.py --host $HOST --port $PORT --user $USER --pwd $PWD --apipwd $HASSIO_TOKEN
