34d9f5f3aee8bdea6938988dae87885fd307805c
korvi <jm.martinez@korvi.net>
2018-07-24 14:35:06 -0500

    remove slash and body

--- lutron_pico/run.sh
+++ lutron_pico/run.sh
@@ -1,4 +1,4 @@
-echo 'Build version: 1.0.18'
+echo 'Build version: 1.0.19'
 echo 'Configuring plugin...'
 CONFIG_PATH=/data/options.json
 HOST="$(jq --raw-output '.host' $CONFIG_PATH)"
@@ -12,5 +12,5 @@ echo "Password: $PWD"
 echo "API password: $HASSIO_TOKEN"
 echo 'Starting service...'
 curl -X GET -H "x-ha-access: $HASSIO_TOKEN" -H "Content-Type: application/json" http://hassio/homeassistant/api/
-curl -X POST -H "x-ha-access: $HASSIO_TOKEN" -H "Content-Type: application/json" http://hassio/homeassistant/api/events/lutron_button_pressed/
+curl -X POST -H "x-ha-access: $HASSIO_TOKEN" -H "Content-Type: application/json" http://hassio/homeassistant/api/events/lutron_button_pressed
 python3 /lutron_pico.py --host $HOST --port $PORT --user $USER --pwd $PWD --apipwd $HASSIO_TOKEN
